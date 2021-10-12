b7ea5b0baed6705d58475d3332f9ec89dc38dc0a
Ann <zzzima12@gmail.com>
2021-10-12 11:19:06 +0300

    исправлены ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 const popup = document.querySelector('.popup');
 const openButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
-const submitButton = document.querySelector('.popup__submit-button');
 const namePage = document.querySelector('.profile__name');
 const descriptionPage = document.querySelector('.profile__description');
-const nameForm = document.querySelector('.popup__name-form');
-const descriptionForm = document.querySelector('.popup__description-form');
+const nameForm = document.querySelector('.popup__input_type_name');
+const descriptionForm = document.querySelector('.popup__input_type_description');
+const form = document.querySelector('.popup__form')
 
 function openPopup() {
-    popup.classList.add('popup_position_isOpened')
+    nameForm.value = namePage.textContent;
+    descriptionForm.value = descriptionPage.textContent;
+    popup.classList.add('popup_opened');
 };
+
 function closePopup() {
-    popup.classList.remove('popup_position_isOpened')
+    popup.classList.remove('popup_opened')
 };
-function form(event) {
+
+function submitForm(event) {
     event.preventDefault();
     namePage.textContent = nameForm.value;
     descriptionPage.textContent = descriptionForm.value;
     closePopup();
-    nameForm.textContent = namePage.value;
-    descriptionForm.textContent = descriptionPage.value;
 }
+
 openButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-submitButton.addEventListener('click', form);
+form.addEventListener('submit', submitForm);
+
 
